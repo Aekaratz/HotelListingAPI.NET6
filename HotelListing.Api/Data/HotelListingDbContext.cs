889fefdb9ec8e34245dadebbf4369a57af9e1750
Mr.Aekarat <102800151+Aekaratz@users.noreply.github.com>
2022-10-14 22:59:50 +0700

    Add project files.

---
+++ HotelListing.Api/Data/HotelListingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Api.Data
+{
+    public class HotelListingDbContext:DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Hotel>Hotels { get; set; }
+        public DbSet<Country>Countries { get; set; }
+    }
+}
